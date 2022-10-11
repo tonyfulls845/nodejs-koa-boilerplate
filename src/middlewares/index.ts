44576c8d0390eb2c1999c8b7d68c963546a51a6e
Luzgin <anton.luzgin@noveogroup.com>
2022-10-11 15:30:11 +0700

    Rename some files

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,4 +1,4 @@
 export * from './validate.middleware';
-export * from './error-handler.middleware';
+export * from './errorHandler.middleware';
 export * from './setUser.middleware';
-export * from './check-post-owner.middleware';
+export * from './checkPostOwner.middleware';
