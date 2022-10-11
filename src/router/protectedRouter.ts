4a9f410711382bfa9a90406a2054b4919cbf03ef
Luzgin <anton.luzgin@noveogroup.com>
2022-10-11 15:38:13 +0700

    Apply lint

--- src/router/protectedRouter.ts
+++ src/router/protectedRouter.ts
@@ -3,7 +3,7 @@ import jwt from 'koa-jwt';
 
 import { JWT_SECRET } from '../config';
 import { apiPrefix } from '../constants/routes';
-import {AppContext, ProtectedAppState} from '../interfaces';
+import { AppContext, ProtectedAppState } from '../interfaces';
 import { setUserMiddleware } from '../middlewares';
 
 export const protectedRouter = new Router<ProtectedAppState, AppContext>({ prefix: apiPrefix });
